037b4ad27c594c2a2f5904be18b686b3f6bb8cf5
yoshixmk <pirori67@gmail.com>
2024-11-10 15:25:18 +0900

    locale mock html

---
+++ routes/(matsu)/matsuken.tsx
@@ -0,0 +1,87 @@
+import { Button } from "../../components/Button.tsx";
+
+export default function Matsuken() {
+  return (
+    <div>
+      <h1 class="text-4xl text-center">Hi, I'm Matsuken</h1>
+      <div class="flex items-center justify-center">
+        <span class="flex">
+          <img
+            src="/matsuken.webp"
+            alt="Matsuken"
+            class="h-32 w-32"
+          />
+          <p class="text-8xl">+</p>
+          {/*<img*/}
+          {/*  src="/costume.webp"*/}
+          {/*  alt="Matsuken costume"*/}
+          {/*  class="h-128 w-128"*/}
+          {/*>*/}
+          {/*</img>*/}
+          <img
+            src="/lemon.webp"
+            alt="Matsuken lemon"
+            class="h-32 w-32"
+          >
+          </img>
+        </span>
+      </div>
+      <Operations/>
+    </div>
+  );
+}
+
+const Operations = () => {
+  return (
+    <div class="flex justify-center">
+      <Button>
+        <span class="flex">
+          <ArrowIcon />Select
+        </span>
+      </Button>
+      <Button>
+        <span class="flex">
+          <MusicIcon />Play
+        </span>
+      </Button>
+    </div>
+  );
+};
+
+const MusicIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+      />
+    </svg>
+  );
+};
+
+const ArrowIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+      />
+    </svg>
+  );
+};
